3b0a5d4b202a7e0fa03edff7b213ecbc43427b75
Amogh Pathak <amogh9792@gmail.com>
2024-04-01 12:25:04 +0530

    logger and exception added

---
+++ source/constant/constant.py
@@ -0,0 +1,18 @@
+# Common Constants
+
+TARGET_COLUMN = 'selling_price'
+TRAIN_PIPELINE_NAME = 'train'
+ARTIFACT_DIR = 'artifact'
+FILE_NAME = 'train_data.csv'
+
+TRAIN_FILE_NAME = 'train.csv'
+TEST_FILE_NAME = 'test.csv'
+
+MONGODB_KEY = "MONGODB_KEY"
+DATABASE_NAME = "car_sales"
+
+# Data Ingestion Constants
+
+TRAIN_DI_COLLECTION_NAME = 'car_dataset'
+DI_DIR_NAME = 'data_ingestion'
+DI_FEATURE_STORE_DIR = "feature_store"
